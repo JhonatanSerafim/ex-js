045d0485e13b0b79d737005c5a2364215222d511
jhonatanserafim <jhonatan.serafim@gmail.com>
2024-02-20 13:25:25 -0300

    add function pagination

--- ex-02/app.js
+++ ex-02/app.js
@@ -1,1 +1,16 @@
-console.log('Oi')
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const previousButton = document.getElementById('previous');
+    const nextButton = document.getElementById('next');
+
+    previousButton.addEventListener('click', function() {
+        window.location.href = '../ex-01/index.html'; // Substitua pelo caminho correto da página anterior
+    });
+
+    nextButton.addEventListener('click', function() {
+        console.log("Abriu a página 2")
+        window.location.href = '../ex-03/index.html'; // Substitua pelo caminho correto da próxima página
+    });
+});
